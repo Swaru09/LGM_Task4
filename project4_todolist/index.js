5701020fe4f6a287637f7a43e9199b21bbaa181c
Swarnali <mukhopadhyayswarnali3@gmail.com>
2023-01-30 20:11:43 +0530

    LGM_task4

---
+++ project4_todolist/index.js
@@ -0,0 +1,23 @@
+document.querySelector('#push').onclick =
+function(){
+    if(document.querySelector("#newtask input").value.length==0){
+        alert("Please enter a task");
+    }
+    else{
+        document.querySelector('#tasks').innerHTML+=`<div class="task"><span id="taskname">${document.querySelector("#newtask input").value}</span><button class="delete">X</button></div>`;
+        var current_tasks=document.querySelectorAll('.delete');
+        for (var i=0;i<current_tasks.length;index++){
+            current_tasks[i].onclick=function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks=document.querySelectorAll('#taskname');
+        for (var i=0;i<tasks.length;index++){
+            tasks[i].onclick=function(){
+                this.classList.toggle('completed');
+            }
+        }
+        document.querySelector('#newtask input').value="";
+    }
+}
